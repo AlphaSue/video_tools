84129627e2f5bef85d133d940ea2a1e339d0d3e2
AlphaSue <409083575@qq.com>
2023-01-16 00:15:46 +0800

    add convert files

---
+++ convert.py
@@ -0,0 +1,41 @@
+#author=AlphaSue
+#date=20230116
+#task='use whisper to auto generate video scripts'
+#methods: python convert.py 苹果通用控制.webm apple2
+
+from datetime import timedelta
+import os
+import whisper
+import sys
+
+def transcribe_audio(path, output):
+    model = whisper.load_model("base") # Change this to your desired model
+    #model = whisper.load_model("tiny") # Change this to your desired model
+    print("Whisper model loaded.")
+    transcribe = model.transcribe(audio=path)
+    segments = transcribe['segments']
+    print(segments)
+    for segment in segments:
+        startTime = str(0)+str(timedelta(seconds=int(segment['start'])))+',000'
+        endTime = str(0)+str(timedelta(seconds=int(segment['end'])))+',000'
+        text = segment['text']
+        segmentId = segment['id']+1
+        segment = f"{segmentId}\n{startTime} --> {endTime}\n{text[1:] if text[0] == ' ' else text}\n\n"
+        print(segment)
+        #srtFilename = os.path.join("SrtFiles", f"VIDEO_FILENAME.srt")
+        srtFilename = output+'.srt'
+        with open(srtFilename, 'a', encoding='utf-8') as srtFile:
+        #with open(output+'.srt', 'a', encoding='utf-8') as srtFile:
+            srtFile.write(segment)
+    return srtFilename
+
+def main():
+    path = sys.argv[1]
+    output = str(sys.argv[2])
+    res = transcribe_audio(path, output)
+    print(res)
+
+if __name__ == '__main__':
+    main()
+
+
