a54fd0163e8e3ca41ccba7257dfd00af6471270c
AlphaSue <409083575@qq.com>
2023-07-09 22:34:43 +0800

    update path

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
-python convert.py data/lex_day.mp4 lex_day.txt
+#python convert.py data/lex_day.mp4 lex_day.txt
 #python convert.py data/mark.mp4 mark.txt
-#python convert.py data/sam.mp4 sam.txt
+conda activate whisper
+python convert.py data/个人工作站介绍.mov pc.txt
+mv pc.txt.srt /mnt/c/Users/Zhongda\ Su/Desktop/
